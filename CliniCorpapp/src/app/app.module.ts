76f366e0fe13b772bc185e48ae777fa680555243
italo pessan <italopessan@gmail.com>
2022-12-02 18:10:30 -0300

    correção app.module.ts

---
+++ CliniCorpapp/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { ConsultaDetalhesComponent } from './consulta/consulta-detalhes/consulta-detalhes.component';
+import { Consulta } from './consulta/models/consulta';
+import { ConsultaResolve } from './consulta/services/consulta.resolve';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HttpClientModule } from '@angular/common/http';
+import { ConsultaListComponent } from './consulta/consulta-list/consulta-list.component';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    ConsultaListComponent,
+    ConsultaDetalhesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
+  ],
+  providers: [ConsultaResolve],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
