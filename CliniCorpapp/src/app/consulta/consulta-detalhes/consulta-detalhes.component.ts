0ca1b60f840aede014f660df16f337da9d2b096d
italo pessan <italopessan@gmail.com>
2022-12-13 15:23:18 -0300

    método voltar

--- CliniCorpapp/src/app/consulta/consulta-detalhes/consulta-detalhes.component.ts
+++ CliniCorpapp/src/app/consulta/consulta-detalhes/consulta-detalhes.component.ts
@@ -1,6 +1,6 @@
 import { editConsulta } from './../models/editarConsulta';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConsultaService } from '../services/consulta.service';
 import { parseDate } from '../services/parseDate';
 
@@ -19,6 +19,7 @@ export class ConsultaDetalhesComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private consultaService: ConsultaService) { this.consulta = this.route.snapshot.data['consulta'] }
 
   ngOnInit(): void {
@@ -33,6 +34,10 @@ export class ConsultaDetalhesComponent implements OnInit {
   updateDate() {
     this.consultaService.updateDate(this.consulta.id, parseDate(this.date))
       .subscribe(sucesso => { console.log(sucesso) })
+  }
+
+  return(){
+    this.router.navigate(["/consultalist"]);
   }
 
 }
