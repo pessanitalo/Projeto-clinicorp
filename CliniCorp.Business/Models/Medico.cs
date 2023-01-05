bb1ffdd60ac870d95690f05194ca3769ae813bd8
pessanitalo <italopessan@gmail.com>
2023-01-05 17:06:55 -0300

    Entidades

--- CliniCorp.Business/Models/Medico.cs
+++ CliniCorp.Business/Models/Medico.cs
@@ -1,15 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ProjetoDemo
+﻿namespace ProjetoDemo
 {
     public class Medico
     {
-        [Key]
         public int Id { get; set; }
         public string Nome { get; set; }
         public int Crm { get; set; }
         public string Cpf { get; set; }
         public Paciente Paciente { get; set; }
         public string Especializacao { get; set; }
+
+        public int ConsultaId { get; set; }
+        public Consulta Consulta { get; set; }
     }
 }
--- CliniCorp.Business/Models/Paciente.cs
+++ CliniCorp.Business/Models/Paciente.cs
@@ -2,7 +2,7 @@
 {
     public class Paciente
     {
-        //entidade dependente
+     
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
