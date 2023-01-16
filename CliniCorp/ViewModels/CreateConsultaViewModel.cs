1eac790ed066d10e24ecf657064c761db0b0ccd6
pessanitalo <italopessan@gmail.com>
2023-01-16 08:37:34 -0300

    mapp

--- CliniCorp/ViewModels/CreateConsultaViewModel.cs
+++ CliniCorp/ViewModels/CreateConsultaViewModel.cs
@@ -3,6 +3,8 @@
     public class CreateConsultaViewModel
     {
         public int Id { get; set; }
+
+        //public int Medico_id { get; set; }
         public DateTime dataConsulta { get; set; }
         public string DescricaoConsulta { get; set; }
         public int StatusConsulta { get; set; }
