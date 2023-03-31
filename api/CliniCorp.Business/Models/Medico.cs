748aff7a44c7b49e2711cce9f04be483b7e79adb
pessanitalo <italopessan@gmail.com>
2023-03-31 15:02:15 -0300

    melhorias classe medico

--- api/CliniCorp.Business/Models/Medico.cs
+++ api/CliniCorp.Business/Models/Medico.cs
@@ -4,9 +4,6 @@ namespace ProjetoDemo
 {
     public class Medico : Pessoa
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Cpf { get; set; }
         public string Especializacao { get; set; }
     }
 }
